0ff4d7f5eda50d0ad8a48ac2be1e91afe0f51f07
Chandima Ranaweera <chan4lk@gmail.com>
2021-08-12 14:54:32 +0530

    initial commit

---
+++ apps/e-doc-booking/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'e-doc-booking',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/e-doc-booking',
+};
